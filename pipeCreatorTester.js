2336c2659559d6326e74029e7ba4ddf642cb7925
Guillaume Besson <guillaume@besson.co>
2020-01-15 17:24:21 +0100

    pipeCreatorTester now can create multiple pipes

--- pipeCreatorTester.js
+++ pipeCreatorTester.js
@@ -1,9 +1,21 @@
 const superagent = require('superagent');
+const _ = require('lodash');
 
 const main = async () => {
   const {body} = await superagent.get(`localhost:8080/state`);
-  const sourceUuid = body.sources[0].uuid;
-  const sinkUuid = body.sinks[0].uuid;
-  await superagent.post(`localhost:8080/source/${sourceUuid}/pipe_to_sink/${sinkUuid}`);
+
+  const possiblePipes = body.sources.flatMap((source) => body.sinks.map((sink) => ({source: source.uuid, sink: sink.uuid})))
+
+  for (const possiblePipe of possiblePipes) {
+    const existingPipe = _.find(body.pipes, {
+      sinkUuid: possiblePipe.sink,
+      sourceUuid: possiblePipe.source,
+    });
+
+    if (!existingPipe) {
+      await superagent.post(`localhost:8080/source/${possiblePipe.source}/pipe_to_sink/${possiblePipe.sink}`);
+    }
+  }
+
 }
 main();
