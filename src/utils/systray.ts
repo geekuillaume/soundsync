9596fc343b5c621604a1a3d818ae0fd61ff197d1
Guillaume Besson <guillaume@besson.co>
2020-02-04 15:48:45 +0100

    Ignore systray error

--- src/utils/systray.ts
+++ src/utils/systray.ts
@@ -19,58 +19,62 @@ export const refreshMenu = async () => {
 
 export const createSystray = () => {
   app.on('ready', () => {
-    const tray = new Tray(resolve(__dirname, '../../res/logo_only.png'));
+    try {
+      const tray = new Tray(resolve(__dirname, '../../res/logo_only.png'));
 
-    const onAutostartClick = async () => {
-      if (await autoLauncher.isEnabled()) {
-        await autoLauncher.disable();
-      } else {
-        await autoLauncher.enable();
-      }
-      await refreshMenu();
-    }
-
-    updateMenu = async () => {
-      const template: any = [
-        { type: 'separator' },
-        { id: 'autostart', label: 'Start on computer startup', type: 'checkbox', click: onAutostartClick, checked: await autoLauncher.isEnabled() },
-        { id: 'exit', label: 'Exit', type: 'normal', click: () => process.exit(0) },
-      ];
-
-      if (isCoordinator()) {
-        template.unshift({ label: 'Started as coordinator', enabled: false })
-      } else if (!getWebrtcServer().coordinatorPeer) {
-        const detectedCoordinators = getDetectedCoordinators();
-        template.unshift(...(detectedCoordinators.length ?
-          detectedCoordinators.map((coordinator) => ({
-            label: `  ${coordinator.host}`,
-            type: 'normal',
-            // @ts-ignore
-            click: () => actAsClientOfCoordinator(`${coordinator.addresses[0]}:${coordinator.port}`)
-          })) :
-          [{ label: '  Scanning...', type: 'normal', enabled: false }])
-        );
-        template.unshift({ label: 'Select a coordinator:', enabled: false });
-        template.unshift({ label: 'Start a new coordinator', click: actAsCoordinator })
-      } else {
-        template.unshift({
-          label: `Connected to ${getWebrtcServer().coordinatorPeer.name}`,
-          enabled: false,
-        });
+      const onAutostartClick = async () => {
+        if (await autoLauncher.isEnabled()) {
+          await autoLauncher.disable();
+        } else {
+          await autoLauncher.enable();
+        }
+        await refreshMenu();
       }
 
-      //@ts-ignore
-      const contextMenu = Menu.buildFromTemplate(template);
+      updateMenu = async () => {
+        const template: any = [
+          { type: 'separator' },
+          { id: 'autostart', label: 'Start on computer startup', type: 'checkbox', click: onAutostartClick, checked: await autoLauncher.isEnabled() },
+          { id: 'exit', label: 'Exit', type: 'normal', click: () => process.exit(0) },
+        ];
 
-      // { label: 'Open controller', type: 'normal', click: () => {
-      //   open('http://127.0.0.1:6512');
-      // } },
+        if (isCoordinator()) {
+          template.unshift({ label: 'Started as coordinator', enabled: false })
+        } else if (!getWebrtcServer().coordinatorPeer) {
+          const detectedCoordinators = getDetectedCoordinators();
+          template.unshift(...(detectedCoordinators.length ?
+            detectedCoordinators.map((coordinator) => ({
+              label: `  ${coordinator.host}`,
+              type: 'normal',
+              // @ts-ignore
+              click: () => actAsClientOfCoordinator(`${coordinator.addresses[0]}:${coordinator.port}`)
+            })) :
+            [{ label: '  Scanning...', type: 'normal', enabled: false }])
+          );
+          template.unshift({ label: 'Select a coordinator:', enabled: false });
+          template.unshift({ label: 'Start a new coordinator', click: actAsCoordinator })
+        } else {
+          template.unshift({
+            label: `Connected to ${getWebrtcServer().coordinatorPeer.name}`,
+            enabled: false,
+          });
+        }
 
-      tray.setContextMenu(contextMenu);
+        //@ts-ignore
+        const contextMenu = Menu.buildFromTemplate(template);
+
+        // { label: 'Open controller', type: 'normal', click: () => {
+        //   open('http://127.0.0.1:6512');
+        // } },
+
+        tray.setContextMenu(contextMenu);
+      }
+
+      tray.setTitle('Soundsync');
+      onDetectionChange(refreshMenu);
+      refreshMenu();
+    } catch (e) {
+
     }
-
-    tray.setTitle('Soundsync');
-    onDetectionChange(refreshMenu);
-    refreshMenu();
   });
 }
