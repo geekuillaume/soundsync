6e119afdd80966951c01efea2051c4364a486939
Guillaume Besson <guillaume@besson.co>
2020-02-05 17:30:15 +0100

    Adds eslint for webui

---
+++ webui/.eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    "import/prefer-default-export": 0,
+    "max-len": 0,
+    "react/prop-types": 0,
+    "no-param-reassign": 0,
+  },
+};
