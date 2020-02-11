9351f71cb2963dddb139d952506e735098cdd900
Guillaume Besson <guillaume@besson.co>
2020-02-11 14:28:17 +0100

    Adds headless compilation for arm

--- scripts/compile_for_raspberrypi.sh
+++ scripts/compile_for_raspberrypi.sh
@@ -5,6 +5,16 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd $DIR
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-docker build . --file ./Dockerfile_crossarch --tag multiarch
+docker build ../ --file ./Dockerfile_crossarch --tag multiarch
 
-docker run -t --rm --workdir /workspace -v `realpath ../`:/workspace multiarch bash -c "yarn && yarn run electron-builder --linux AppImage --armv7l"
+docker run -it --rm --workdir /workspace \
+  -v `realpath ../app`:/workspace/app_tmp \
+  -v `realpath ../webui`:/workspace/webui \
+  -v `realpath ../res`:/workspace/res \
+  -v `realpath ../bin`:/workspace/bin \
+  multiarch bash -c "cp -R app_tmp app && \
+   rm -rf app/node_modules && \
+   mv /tmp/workspace/app/node_modules app/node_modules && \
+   cp /tmp/workspace/package.json ./ && \
+   mv /tmp/workspace/node_modules ./ && \
+   yarn run pack:headless:arm"
