dab90e4eaf95d7446c2858e608886e526dcd7b2e
Guillaume Besson <guillaume@besson.co>
2020-03-23 17:53:20 +0100

    Fix compile for rpi script and dockerfile

--- scripts/compile_for_raspberrypi.sh
+++ scripts/compile_for_raspberrypi.sh
@@ -8,15 +8,7 @@ docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker build ../ --file ./Dockerfile_crossarch --tag multiarch
 
 docker run -it --rm --workdir /workspace \
-  -v `realpath ../app`:/workspace/app_tmp \
-  -v `realpath ../webui`:/workspace/webui \
-  -v `realpath ../res`:/workspace/res \
-  -v `realpath ../bin`:/workspace/bin \
-  -v `realpath ../scripts`:/workspace/scripts \
+  -v `realpath ../`:/workspace \
   -e GITHUB_TOKEN=$GITHUB_TOKEN \
-  multiarch bash -c "cp -R app_tmp app && \
-   rm -rf app/node_modules && \
-   mv /tmp/workspace/app/node_modules app/node_modules && \
-   cp /tmp/workspace/package.json ./ && \
-   mv /tmp/workspace/node_modules ./ && \
+  multiarch bash -c "yarn && \
    yarn run pack -l deb --armv7l"
