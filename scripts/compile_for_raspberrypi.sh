e436e5ab65d12476c6aa772208473fb27b1c5c36
Guillaume Besson <guillaume@besson.co>
2020-03-30 15:11:01 +0200

    Deploy arm package on tag with github actions

--- scripts/compile_for_raspberrypi.sh
+++ scripts/compile_for_raspberrypi.sh
@@ -7,8 +7,18 @@ cd $DIR
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker build ../ --file ./Dockerfile_crossarch --tag multiarch
 
-docker run --rm --workdir /workspace \
-  -v `realpath ../`:/workspace \
-  -e GITHUB_TOKEN=$GITHUB_TOKEN \
-  multiarch bash -c "yarn && yarn build && \
-   yarn run pack -l deb --armv7l"
+if [ "$IS_GITHUB_RELEASE" == "true" ]
+then
+  docker run --rm --workdir /workspace \
+    -v `realpath ../`:/workspace \
+    -e GITHUB_TOKEN=$GITHUB_TOKEN \
+    multiarch bash -c "yarn && yarn build && \
+    yarn run pack -l deb --armv7l -p"
+else
+  docker run --rm --workdir /workspace \
+    -v `realpath ../`:/workspace \
+    -e GITHUB_TOKEN=$GITHUB_TOKEN \
+    multiarch bash -c "yarn && yarn build && \
+    yarn run pack -l deb --armv7l"
+fi
+
