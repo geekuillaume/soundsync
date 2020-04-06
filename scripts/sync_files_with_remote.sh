3d18efdaddec3d1433ec52f661337d0b46eedd52
Guillaume Besson <guillaume@besson.co>
2020-04-06 15:06:35 +0200

    Simplify sync script for dev on distant host

--- scripts/sync_files_with_remote.sh
+++ scripts/sync_files_with_remote.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-
+set -e
 # This is used when working with a distant ssh peer which has rsync
 # first argument needs to be the ssh connection string ([user@]host)
 
-rsync -avz --exclude node_modules --exclude .cache ./{bin,src,app,webui,package.json,yarn.lock} $1
-while inotifywait -r -e modify,create,delete,move ./src ./app ./package.json ./webui; do
-    wait 1
-    rsync -avz --exclude node_modules --exclude .cache ./{bin,src,app,webui,package.json,yarn.lock} $1
+while true; do
+    sleep 1
+    rsync -avz --exclude node_modules --exclude .cache ./{src,app,webui,package.json,yarn.lock} $1
+    inotifywait -r -e modify,create,delete,move ./src ./app ./package.json ./webui
 done
