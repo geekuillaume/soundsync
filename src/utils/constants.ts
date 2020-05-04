5bb3d1e510ba8ebc9a99a3d08551d47a090d4465
Guillaume Besson <guillaume@besson.co>
2020-05-04 15:15:30 +0200

    Increase reliability of webrtc audio channel

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -7,8 +7,8 @@ export const NO_RESPONSE_TIMEOUT = 5000; // if there if no response from a wrtc 
 export const HEARTBEAT_INTERVAL = 300; // send a heartbeat every 300 ms
 export const HEARTBEAT_JITTER = 100; // randomize heartbeat sending interval between peers
 export const AUDIO_CHANNEL_OPTIONS: RTCDataChannelInit = {
-  ordered: false,
-  maxPacketLifeTime: 500,
+  ordered: true,
+  maxPacketLifeTime: 1000,
 };
 export const ICE_GATHERING_TIMEOUT = 20000;
 
