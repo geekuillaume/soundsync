8fd3cd5df9df45dd31c76b9bc1980abd81989ba2
Guillaume Besson <guillaume@besson.co>
2020-05-08 12:45:12 +0200

    Improves peer list view on webui

--- webui/src/components/PeersList.jsx
+++ webui/src/components/PeersList.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Dialog from '@material-ui/core/Dialog';
 import {
-  makeStyles, DialogTitle, DialogContent, List, ListItem, ListItemText, ListItemAvatar,
+  makeStyles, DialogTitle, ListSubheader, List, ListItem, ListItemText, ListItemAvatar,
 } from '@material-ui/core';
 import DevicesIcon from '@material-ui/icons/Devices';
 import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline';
 import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
 import AdjustIcon from '@material-ui/icons/Adjust';
 import RemoveCircleOutlineIcon from '@material-ui/icons/RemoveCircleOutline';
-import { usePeers, usePeersManager } from '../utils/useSoundSyncState';
+import { usePeersManager } from '../utils/useSoundSyncState';
 
 const useStyles = makeStyles({
   dialog: {
@@ -18,35 +18,59 @@ const useStyles = makeStyles({
 
 export const PeersListDialog = ({ open, onClose }) => {
   const styles = useStyles();
-  const peersManager = usePeersManager();
-  const peers = Object.values(peersManager.peers);
+  const peers = usePeersManager().peers.filter((peer) => peer.state !== 'deleted');
+
+  const connectedPeers = peers.filter((peer) => peer.state === 'connected');
+  const otherPeers = peers.filter((peer) => peer.state !== 'connected');
 
   return (
-    <Dialog open={open} onClose={onClose} maxWidth="sm" className={styles.dialog}>
+    <Dialog
+      open={open}
+      onClose={onClose}
+      maxWidth="sm"
+      fullWidth
+      className={styles.dialog}
+    >
       <DialogTitle>Soundsync devices</DialogTitle>
-      <DialogContent>
-        <List>
-          {peers.map((peer) => (
-            <ListItem key={peer.uuid}>
-              <ListItemAvatar>
-                {peer.state === 'connected'
-                  ? <CheckCircleOutlineIcon />
-                  : peer.state === 'connecting'
-                    ? <AdjustIcon />
-                    : peer.state === 'deleted'
-                      ? <RemoveCircleOutlineIcon />
-                      : <ErrorOutlineIcon />}
-              </ListItemAvatar>
-              <ListItemText
-                primary={peer.name}
-                secondary={peer.uuid}
-              />
-            </ListItem>
-          ))}
-        </List>
-        <button onClick={() => { console.log(peersManager); }}>Log PeersManager</button>
-      </DialogContent>
+      <List>
+        <ListSubheader>Connected devices</ListSubheader>
+        {connectedPeers.map((peer) => (
+          <ListItem key={peer.uuid} button>
+            <ListItemAvatar>
+              {peer.state === 'connected'
+                ? <CheckCircleOutlineIcon />
+                : peer.state === 'connecting'
+                  ? <AdjustIcon />
+                  : peer.state === 'deleted'
+                    ? <RemoveCircleOutlineIcon />
+                    : <ErrorOutlineIcon />}
+            </ListItemAvatar>
+            <ListItemText
+              primary={peer.name}
+              secondary={window.localStorage.getItem('soundsync:debug') && peer.uuid}
+            />
+          </ListItem>
+        ))}
+        {otherPeers.length !== 0 && <ListSubheader>Other devices</ListSubheader>}
+        {otherPeers.map((peer) => (
+          <ListItem key={peer.uuid} button>
+            <ListItemAvatar>
+              {peer.state === 'connected'
+                ? <CheckCircleOutlineIcon />
+                : peer.state === 'connecting'
+                  ? <AdjustIcon />
+                  : peer.state === 'deleted'
+                    ? <RemoveCircleOutlineIcon />
+                    : <ErrorOutlineIcon />}
+            </ListItemAvatar>
+            <ListItemText
+              primary={peer.name}
+              secondary={window.localStorage.getItem('soundsync:debug') && peer.uuid}
+            />
+          </ListItem>
+        ))}
+      </List>
+      {window.localStorage.getItem('soundsync:debug') && <button onClick={() => { console.log(peersManager); }}>Log PeersManager</button>}
     </Dialog>
-
   );
 };
