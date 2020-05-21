040105cd267bc3b0a207d1c076105a66c4c41863
Guillaume Besson <guillaume@besson.co>
2020-05-21 18:50:48 +0200

    Adds download link to webui not connected page

--- webui/src/components/app.jsx
+++ webui/src/components/app.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import { Provider, CachePolicies } from 'use-http';
 import { SoundState } from './SoundState';
-import { SoundSyncProvider } from '../utils/useSoundSyncState';
+import { SoundSyncProvider, useIsConnected } from '../utils/useSoundSyncState';
 import { Header } from './Header';
+import { FirstUse } from './FirstUse/FirstUse';
 
 // port 1234 is dev server of Parcel, when using it we use the local soundsync server
 const API_URL = document.location.host.endsWith(':1234') ? 'http://localhost:6512' : `http://${document.location.host}`;
 
-export const App = () => (
-  <Provider
-    url={API_URL}
-    options={{ cachePolicy: CachePolicies.NO_CACHE }}
-  >
-    <SoundSyncProvider>
-      <Header />
-      <SoundState />
-    </SoundSyncProvider>
-  </Provider>
-);
+export const App = () => {
+  const isConnected = useIsConnected();
+
+  return (
+    <Provider
+      url={API_URL}
+      options={{ cachePolicy: CachePolicies.NO_CACHE }}
+    >
+      <SoundSyncProvider>
+        <Header />
+        <SoundState />
+        {!isConnected && <FirstUse />}
+      </SoundSyncProvider>
+    </Provider>
+  );
+};
