e44a1c37a40800e31a585cc04693f67701042991
Guillaume Besson <guillaume@besson.co>
2020-05-23 13:51:52 +0200

    Adapt first use for small screens

--- webui/src/components/FirstUse/FirstUse.jsx
+++ webui/src/components/FirstUse/FirstUse.jsx
@@ -19,18 +19,18 @@ export const FirstUse = () => {
     <div className="container firstUseContainer">
       <p>Soundsync is scanning your local network for Soundsync enabled devices. Make sure Soundsync is started on your computer and that you are connected to the same network / wifi as the other devices.</p>
       <h4>Download Soundsync:</h4>
-      <div className="downloadPlateforms">
-        <div className="plateform">
+      <div className="downloadPlatforms">
+        <div className="platform">
           <img src={WindowsIcon} />
           <h3>Windows</h3>
           <Button variant="outlined" className="downloadButton" href="https://github.com/geekuillaume/soundsync/releases/download/bleeding-edge/Soundsync_Setup_0.1.0.exe">Download</Button>
         </div>
-        <div className="plateform">
+        <div className="platform">
           <img src={MacOsIcon} />
           <h3>MacOS</h3>
           <Button variant="outlined" className="downloadButton" href="https://github.com/geekuillaume/soundsync/releases/download/bleeding-edge/Soundsync-0.1.0.dmg">Download</Button>
         </div>
-        <div className="plateform">
+        <div className="platform">
           <img src={LinuxIcon} />
           <h3>Linux</h3>
           <Button variant="outlined" className="downloadButton" href="https://github.com/geekuillaume/soundsync/releases/download/bleeding-edge/soundsync_0.1.0_amd64.deb">
@@ -42,7 +42,7 @@ export const FirstUse = () => {
             <span className="packageInfo">.pacman for Archlinux</span>
           </Button>
         </div>
-        <div className="plateform">
+        <div className="platform">
           <img src={RaspberryIcon} />
           <h3>Linux ARM (Raspberry)</h3>
           <Button variant="outlined" className="downloadButton" href="https://github.com/geekuillaume/soundsync/releases/download/bleeding-edge/soundsync_0.1.0_armv7l.deb">
