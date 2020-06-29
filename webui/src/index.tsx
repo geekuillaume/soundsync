0bb4cc41a18f6b45b605d3da5c461d926f40b2a8
Guillaume Besson <guillaume@besson.co>
2020-06-29 13:31:19 +0200

    Adds a util function window.soundsyncDebug to activate debug mode

--- webui/src/index.tsx
+++ webui/src/index.tsx
@@ -8,3 +8,10 @@ import '../../src/utils/sentry';
 
 const mountNode = document.getElementById('app');
 ReactDOM.render(<App />, mountNode);
+
+// @ts-ignore
+window.soundsyncDebug = () => {
+  localStorage.debug = "soundsync,soundsync:*,-soundsync:timekeeper,-soundsync:*:timekeepResponse,-soundsync:*:timekeepRequest,-soundsync:*:peerDiscovery,-soundsync:api,-soundsync:wrtcPeer:*:soundState,-soundsync:*:librespot,-soundsync:*:peerSoundState,-soundsync:*:peerConnectionInfo";
+  localStorage['soundsync:debug'] = true;
+  document.location.reload();
+}
