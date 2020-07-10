63a5936ef4a42d622e4c5e6b4fbad8a97613a4b4
Guillaume Besson <guillaume@besson.co>
2020-07-10 11:17:26 +0200

    Disable sentry in dev mode

--- src/utils/sentry.ts
+++ src/utils/sentry.ts
@@ -12,10 +12,12 @@ if (process.browser) {
 }
 
 try {
-  Sentry.init({
-    dsn: 'https://01d9c8f4220e4107992cfc3599c2f8e1@o403236.ingest.sentry.io/5265532',
-    release: `soundsync_desktop_${BUILD_VERSION}`,
-  });
+  if (!process.env.DEV_MODE) {
+    Sentry.init({
+      dsn: 'https://01d9c8f4220e4107992cfc3599c2f8e1@o403236.ingest.sentry.io/5265532',
+      release: `soundsync_desktop_${BUILD_VERSION}`,
+    });
+  }
 } catch (e) {
   // do nothing as it is caused by electron not being available
 }
