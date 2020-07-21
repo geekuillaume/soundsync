e2161751e8daaaf8709a93807903259f918cce3b
Guillaume Besson <guillaume@besson.co>
2020-07-21 15:08:31 +0200

    Again github action fix

--- scripts/compile_for_raspberrypi.sh
+++ scripts/compile_for_raspberrypi.sh
@@ -7,18 +7,8 @@ cd $DIR
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker build ../ --file ./Dockerfile_crossarch --tag multiarch
 
-if [ "$IS_GITHUB_RELEASE" == "true" ]
-then
-  docker run --rm --workdir /workspace \
-    -v `realpath ../`:/workspace \
-    -e GITHUB_TOKEN=$GITHUB_TOKEN \
-    multiarch bash -c "yarn && yarn build && \
-    yarn run pack -l deb --armv7l -p"
-else
-  docker run --rm --workdir /workspace \
-    -v `realpath ../`:/workspace \
-    -e GITHUB_TOKEN=$GITHUB_TOKEN \
-    multiarch bash -c "yarn && yarn build && \
-    yarn run pack -l deb --armv7l"
-fi
+docker run --rm --workdir /workspace \
+  -v `realpath ../`:/workspace \
+  multiarch bash -c "yarn && yarn build && \
+  yarn run pack -l deb --armv7l"
 
