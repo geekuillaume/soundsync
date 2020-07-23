3472512545c4c034581809ec77c089e33ecfc882
Guillaume Besson <guillaume@besson.co>
2020-07-23 18:55:36 +0200

    clean .ico file before uploading release artifact

--- scripts/clean_build.sh
+++ scripts/clean_build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rm -rf bin/*-unpacked bin/builder-effective-config.yaml bin/*.blockmap bin/*.yml bin/mac/*
+rm -rf bin/*-unpacked bin/builder-effective-config.yaml bin/*.blockmap bin/*.yml bin/mac/* bin/.icon-ico
 (for f in bin/*\ *; do mv "$f" "${f// /_}"; done) || true
