a7fa1cc288c17c62f9b6813427ec04dc94fdd745
Guillaume Besson <guillaume@besson.co>
2020-07-24 12:51:34 +0200

    Commented WIP about mdns

--- src/utils/network/mdns.ts
+++ src/utils/network/mdns.ts
@@ -1,108 +1,121 @@
-/* eslint-disable no-continue */
-import debug from 'debug';
-import util from 'util';
-import os from 'os';
-import dgram from 'dgram';
-import { EventEmitter } from 'events';
+// THIS IS A WORK IN PROGRESS
+// I started implementing a mdns service file to use it a hinting service to communicate the rendez-vous uuid
+// for web browsers on a network that disallow LAN DNS entries. After some testings, a lot of browsers/OS don't support
+// mdns so I stopped working on this. In the future, it will still be an interesting feature to add
 
-import dns from 'dns-js';
+// /* eslint-disable no-continue */
+// import debug from 'debug';
+// import util from 'util';
+// import os from 'os';
+// import dgram from 'dgram';
+// import { EventEmitter } from 'events';
 
-const log = debug('soundsync:mdns');
+// import dns, { DNSPacket, DNSRecord } from 'dns-js';
 
-const DNSPacket = dns.DNSPacket;
-//var DNSRecord = dns.DNSRecord;
+// const log = debug('soundsync:mdns');
 
-const MDNS_MULTICAST = '224.0.0.251';
+// const DNSPacket = dns.DNSPacket;
+// //var DNSRecord = dns.DNSRecord;
 
-interface MdnsConnections {
-  socket: dgram.Socket;
-}
+// const MDNS_MULTICAST = '224.0.0.251';
 
-export class Mdns extends EventEmitter {
-  connections: MdnsConnections[] = [];
+// interface MdnsConnections {
+//   socket: dgram.Socket;
+// }
 
-  constructor() {
-    super();
-  }
+// export class Mdns extends EventEmitter {
+//   connections: MdnsConnections[] = [];
 
-  async start() {
-    const interfaces = os.networkInterfaces();
-    const interfacesNames = Object.keys(interfaces);
-    await Promise.all(interfacesNames.map(async (interfaceName) => {
-      const addresses = interfaces[interfaceName];
-      await Promise.all(addresses.map(async (address) => {
-        if (address.internal || address.address.indexOf(':') !== -1) {
-          return;
-        }
-        await this.createSocket(address.address, 0);
-      }));
-    }));
-    await this.createSocket('0.0.0.0', 5353);
-  }
+//   constructor() {
+//     super();
+//   }
 
-  async createSocket(address: string, port: number) {
-    const socket = dgram.createSocket({
-      type: 'udp4',
-      reuseAddr: true,
-    });
-    socket.unref();
-    await new Promise((resolve, reject) => {
-      socket.on('error', (err) => {
-        log(`Error while binding to address ${address} and port ${port}`);
-        reject(err);
-      });
-      socket.on('listening', () => {
-        log(`Bound to address ${address} and port ${port}`);
-        resolve();
-      });
-      socket.bind(port, address);
-    });
-    if (port === 5353) {
-      socket.addMembership(MDNS_MULTICAST);
-      socket.setMulticastTTL(255);
-      socket.setMulticastLoopback(true);
-    }
+//   async start() {
+//     const interfaces = os.networkInterfaces();
+//     const interfacesNames = Object.keys(interfaces);
+//     await Promise.all(interfacesNames.map(async (interfaceName) => {
+//       const addresses = interfaces[interfaceName];
+//       await Promise.all(addresses.map(async (address) => {
+//         if (address.internal || address.address.indexOf(':') !== -1) {
+//           return;
+//         }
+//         await this.createSocket(address.address, 0);
+//       }));
+//     }));
+//     await this.createSocket('0.0.0.0', 5353);
+//   }
 
-    socket.on('message', (message, remote) => {
-      let packets;
-      try {
-        packets = dns.DNSPacket.parse(message);
-        if (!(packets instanceof Array)) {
-          packets = [packets];
-        }
-      } catch (er) {
-        //partial, skip it
-        // debug('packet parsing error', er);
-        return;
-      }
-      packets.forEach((packet) => {
-        const questionsName = packet.question.map(({ name }) => name).filter((name) => name.includes('soundsync.local'));
-        if (questionsName.length) {
-          console.log(questionsName);
-        }
-      });
-      this.connections.push({ socket });
-    });
-  }
+//   async createSocket(address: string, port: number) {
+//     const socket = dgram.createSocket({
+//       type: 'udp4',
+//       reuseAddr: true,
+//     });
+//     socket.unref();
+//     await new Promise((resolve, reject) => {
+//       socket.on('error', (err) => {
+//         log(`Error while binding to address ${address} and port ${port}`);
+//         reject(err);
+//       });
+//       socket.on('listening', () => {
+//         log(`Bound to address ${address} and port ${port}`);
+//         resolve();
+//       });
+//       socket.bind(port, address);
+//     });
+//     if (port === 5353) {
+//       socket.addMembership(MDNS_MULTICAST);
+//       socket.setMulticastTTL(255);
+//       socket.setMulticastLoopback(true);
+//     }
+//     this.connections.push({ socket });
 
-  async send(packet) {
-    const buf = DNSPacket.toBuffer(packet);
-    // this.connections.forEach(onEach);
-    await Promise.all(this.connections.map(async ({ socket }) => new Promise((resolve, reject) => {
-      socket.send(buf, 0, buf.length, 5353, '224.0.0.251', (err, bytes) => {
-        if (err) {
-          reject(err);
-        } else {
-          log('%s sent %d bytes with err:%s', socket.address().address, bytes, err);
-          resolve(bytes);
-        }
-      });
-    })));
-  }
-}
+//     socket.on('message', (message) => {
+//       let packets;
+//       try {
+//         packets = dns.DNSPacket.parse(message);
+//         if (!(packets instanceof Array)) {
+//           packets = [packets];
+//         }
+//       } catch (er) {
+//         //partial, skip it
+//         // debug('packet parsing error', er);
+//         return;
+//       }
+//       const usefulPackets = packets.filter((packet) => {
+//         const questionsName = packet.question.map(({ name }) => name).filter((name) => name.includes('soundsync'));
+//         return !!questionsName.length;
+//       });
+//       if (!usefulPackets.length) {
+//         return;
+//       }
+//       console.log(JSON.stringify(usefulPackets, null, 2));
+//     });
+//   }
 
-const mdns = new Mdns();
-mdns.start();
-setTimeout(() => {
-  process.exit(0);
-}, 300000);
+//   async sendRequest(name) {
+//     const packet = new DNSPacket();
+//     packet.question.push(new DNSRecord(name, DNSRecord.Type.ANY, 1));
+
+//     await this.send(packet);
+//   }
+
+//   async send(packet) {
+//     const buf = DNSPacket.toBuffer(packet);
+//     await Promise.all(this.connections.map(async ({ socket }) => new Promise((resolve, reject) => {
+//       socket.send(buf, 0, buf.length, 5353, '224.0.0.251', (err, bytes) => {
+//         if (err) {
+//           reject(err);
+//         } else {
+//           log('%s sent %d bytes with err:%s', socket.address().address, bytes, err);
+//           resolve(bytes);
+//         }
+//       });
+//     })));
+//   }
+// }
+
+// const mdns = new Mdns();
+// mdns.start();
+// setTimeout(() => {
+//   process.exit(0);
+// }, 300000);
