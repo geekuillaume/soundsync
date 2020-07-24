62f920a8409054478cf26760ce7fed7526f043ec
Guillaume Besson <guillaume@besson.co>
2020-07-24 10:27:21 +0200

    Rename installers to lowercase before uploading

--- scripts/clean_build.sh
+++ scripts/clean_build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 rm -rf bin/*-unpacked bin/builder-effective-config.yaml bin/*.blockmap bin/*.yml bin/mac/* bin/.icon-ico
+# Remove whitespace from file names
 (for f in bin/*\ *; do mv "$f" "${f// /_}"; done) || true
+# Remove uppercase from file names
+(for f in bin/*; do mv "$f" "`echo $f | tr 'A-Z' 'a-z'`"; done) || true
