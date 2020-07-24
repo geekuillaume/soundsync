90eea193aa6269eb1ec7eb0426324e0bf914a9f5
Guillaume Besson <guillaume@besson.co>
2020-07-24 11:14:46 +0200

    Updates install links on webui

--- webui/src/components/FirstUse/DownloadLinks.jsx
+++ webui/src/components/FirstUse/DownloadLinks.jsx
@@ -13,21 +13,21 @@ export const DownloadLinks = ({ twoLinesLayout }) => (
     <div className="platform">
       <img src={WindowsIcon} />
       <h3>Windows</h3>
-      <Button variant="outlined" className="downloadButton" href="https://github.com/geekuillaume/soundsync/releases/download/bleeding-edge/Soundsync_Setup_0.1.0.exe">Download</Button>
+      <Button variant="outlined" className="downloadButton" href="https://github.com/geekuillaume/soundsync/releases/download/bleeding-edge/Soundsync_Setup_0.2.1.exe">Download</Button>
     </div>
     <div className="platform">
       <img src={MacOsIcon} />
       <h3>MacOS</h3>
-      <Button variant="outlined" className="downloadButton" href="https://github.com/geekuillaume/soundsync/releases/download/bleeding-edge/Soundsync-0.1.0.dmg">Download</Button>
+      <Button variant="outlined" className="downloadButton" href="https://github.com/geekuillaume/soundsync/releases/download/bleeding-edge/soundsync-0.2.1.dmg">Download</Button>
     </div>
     <div className="platform">
       <img src={LinuxIcon} />
       <h3>Linux</h3>
-      <Button variant="outlined" className="downloadButton" href="https://github.com/geekuillaume/soundsync/releases/download/bleeding-edge/soundsync_0.1.0_amd64.deb">
+      <Button variant="outlined" className="downloadButton" href="https://github.com/geekuillaume/soundsync/releases/download/bleeding-edge/soundsync-amd64-0.2.1.deb">
         <span>Download</span>
         <span className="packageInfo">.deb for Ubuntu/Debian</span>
       </Button>
-      <Button variant="outlined" className="downloadButton" href="https://github.com/geekuillaume/soundsync/releases/download/bleeding-edge/soundsync-0.1.0.pacman">
+      <Button variant="outlined" className="downloadButton" href="https://github.com/geekuillaume/soundsync/releases/download/bleeding-edge/soundsync-x64-0.2.1.pacman">
         <span>Download</span>
         <span className="packageInfo">.pacman for Archlinux</span>
       </Button>
@@ -35,7 +35,7 @@ export const DownloadLinks = ({ twoLinesLayout }) => (
     <div className="platform">
       <img src={RaspberryIcon} />
       <h3>Linux ARM (Raspberry)</h3>
-      <Button variant="outlined" className="downloadButton" href="https://github.com/geekuillaume/soundsync/releases/download/bleeding-edge/soundsync_0.1.0_armv7l.deb">
+      <Button variant="outlined" className="downloadButton" href="https://github.com/geekuillaume/soundsync/releases/download/bleeding-edge/soundsync-armv7l-0.2.1.deb">
         <span>Download</span>
         <span className="packageInfo">.deb for Raspbian</span>
       </Button>
