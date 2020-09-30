6fd0edcb9c3f87395178541ebebfd801489b06c3
Guillaume Besson <guillaume@besson.co>
2020-09-30 14:44:04 +0200

    🐛 Handles all ways of exiting node process in on_exit handler

--- src/utils/on_exit.ts
+++ src/utils/on_exit.ts
@@ -1,19 +1,31 @@
+import nodeCleanup from 'node-cleanup';
+
 const callbacks = [];
 export const onExit = (callback) => {
   callbacks.push(callback);
 };
 
-export const exit = async (code: number, async = false) => {
+export const exit = async (code: number, async = false, signal?: any) => {
   const exitPromise = Promise.all(callbacks.map((callback) => callback()));
   if (async) {
+    // TODO: debug this to wait for all promises on Ctrl+C
     await exitPromise;
   }
-  process.exit(code);
+  if (signal) {
+    process.kill(process.pid, signal);
+  } else {
+    process.exit(code);
+  }
 };
 
 // @ts-ignore
 if (!process.browser) {
-  process.on('SIGINT', () => {
-    exit(0, true);
+  nodeCleanup((exitCode, signal) => {
+    if (signal) {
+      exit(exitCode, true);
+      nodeCleanup.uninstall();
+      return false;
+    }
+    return true;
   });
 }
