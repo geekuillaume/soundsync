9f8b6fa17ba15440bcbbc2eb68cc9f091cbe4f2b
Guillaume Besson <guillaume@besson.co>
2020-10-12 10:35:33 +0200

    🐛 Fix RTCPeerConnection not a constructor error

--- webui/src/utils/hasNecessaryApi.ts
+++ webui/src/utils/hasNecessaryApi.ts
@@ -1,12 +1,12 @@
 export const hasNecessaryApi = () => {
   if (
-    typeof RTCPeerConnection === undefined ||
+    typeof window.RTCPeerConnection === undefined ||
     typeof localStorage === undefined ||
     typeof WebAssembly === undefined ||
     typeof WebAssembly.instantiate === undefined ||
     typeof localStorage === undefined
   ) {
-    return false
+    return false;
   }
   return true;
 }
