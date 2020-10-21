7827b67638fa9f7ef7f3d5a422f51a7b06d56f10
Guillaume Besson <guillaume@besson.co>
2020-10-21 13:05:12 +0200

    🐛 Don't crash on updater error

--- src/utils/environment/updater.ts
+++ src/utils/environment/updater.ts
@@ -6,16 +6,20 @@ import { onElectronReady } from './electron';
 const l = debug(`soundsync:updater`);
 
 export const installAutoUpdater = () => {
-  onElectronReady(() => {
+  onElectronReady(async () => {
     autoUpdater.logger = {
       info: l,
       warn: l,
       error: l,
     };
     l(`Updater starting, current version: ${autoUpdater.currentVersion}`);
-    autoUpdater.checkForUpdatesAndNotify();
     autoUpdater.on('update-available', (info) => {
       l(`Update available: ${info.version}`);
     });
+    try {
+      await autoUpdater.checkForUpdatesAndNotify();
+    } catch (e) {
+      l(`Error while checking for update: ${e.message}`);
+    }
   });
 };
