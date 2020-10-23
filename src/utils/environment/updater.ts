a086bd3f83b8e513216c85b36d2db83ea133dd1d
Guillaume Besson <guillaume@besson.co>
2020-10-23 14:46:57 +0200

    🐛 Don't crash on electron update error

--- src/utils/environment/updater.ts
+++ src/utils/environment/updater.ts
@@ -12,12 +12,22 @@ export const installAutoUpdater = () => {
       warn: log,
       error: log,
     };
+    // autodownload crash process if there is an error during the new version download, we need to download it manually to catch the error
+    autoUpdater.autoDownload = false;
     log(`Updater starting, current version: ${autoUpdater.currentVersion}`);
-    autoUpdater.on('update-available', (info) => {
+    autoUpdater.on('update-available', async (info) => {
       log(`Update available: ${info.version}`);
+      try {
+        await autoUpdater.downloadUpdate();
+      } catch (e) {
+        // ignore error, this will be logged by on('error') handler
+      }
+    });
+    autoUpdater.on('error', (e) => {
+      log(`Error while updating`, e);
     });
     try {
-      await autoUpdater.checkForUpdatesAndNotify();
+      await autoUpdater.checkForUpdates();
     } catch (e) {
       log(`Error while checking for update: ${e.message}`);
     }
